a93d1f498d37360207854e23c4715fdd95d43956
David Jelenc <david.jelenc@fri.uni-lj.si>
2023-11-13 18:09:31 +0100

    Prepares assignments

--- java-integrity/src/main/java/vp/integrity/CommunicationExampleHMAC.java
+++ java-integrity/src/main/java/vp/integrity/CommunicationExampleHMAC.java
@@ -25,22 +25,11 @@ public class CommunicationExampleHMAC {
         // Simulacijsko okolje, v katerem bivajo agenti
         final Environment env = new Environment();
 
-        // Globalno ustvarimo simetrični ključ, ki ga Ana in Bor uporabita za preverjanje MAC
-        final SecretKey key = KeyGenerator.getInstance("HmacSHA256").generateKey();
-
         env.add(new Agent("ana") {
             @Override
             public void task() throws Exception {
                 final byte[] pt = "Zdravo Bor, tukaj Ana.".getBytes(StandardCharsets.UTF_8);
                 send("bor", pt);
-
-                // Ana instanciira algoritem MAC in izračuna značko
-                final Mac mac = Mac.getInstance("HmacSHA256");
-                mac.init(key);
-                final byte[] tag = mac.doFinal(pt);
-                // poleg sporočila pošlje še značko
-                send("bor", tag);
-
                 print("Poslala sem sporočilo in značko.");
             }
         });
@@ -50,18 +39,6 @@ public class CommunicationExampleHMAC {
             public void task() throws Exception {
                 // Bor prejme sporočilo
                 final byte[] pt = receive("ana");
-
-                // Bor prejme še značko in sporočilo preveri
-                final byte[] tag = receive("ana");
-
-                final Mac mac = Mac.getInstance("HmacSHA256");
-                mac.init(key);
-
-                final byte[] recomputedTag = mac.doFinal(pt);
-
-                if (!MessageDigest.isEqual(recomputedTag, tag)) {
-                    print("Sporočilo ni veljavno, prekinjam program.");
-                }
 
                 // Bor sporočilo izpiše
                 print("Sporočilo se glasi: '%s'", new String(pt, StandardCharsets.UTF_8));
